4c58abad38f3210397f085cd03438785bdf1b4d8
oso082024 <oso082024@gmail.com>
2024-11-29 10:35:56 +0100

    Added menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
-﻿namespace TrenaryTranslator;
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace TrenaryTranslator;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("1. Ternary Translations");
-        Console.Write("type in your ternary text: ");
-        Translators Translate = new Translators();
-        Console.WriteLine(Translate.TernaryToAscii(Console.ReadLine() ?? "0"));
+        bool exit = false;
+        while(!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Ternary Translations");
+            Console.WriteLine("0. Exit");
+            ConsoleKey choice = Console.ReadKey().Key;
+            switch(choice)
+            {
+                case ConsoleKey.D1:
+                TernaryMenu();
+                break;
+                case ConsoleKey.D0:
+                    exit = true;
+                    break;
+            }
+        }
     }
+    static void TernaryMenu()
+    {
+        while(true)
+        {
+            Console.Clear();
+            Console.WriteLine("1. To Ternary");
+            Console.WriteLine("2. From Ternary");
+            ConsoleKey choice = Console.ReadKey().Key;
+            switch (choice)
+            {
+                case ConsoleKey.D0:
+                    return;
+                case ConsoleKey.D1:
+                    break;
+                case ConsoleKey.D2:
+                    Console.Write("Type in your ternary text: ");
+                    Translators Translate = new Translators();
+                    Console.WriteLine(Translate.TernaryToAscii(Console.ReadLine() ?? "0"));
+                    
+                    break;
+            }
+        }
+    }
+    
 }
